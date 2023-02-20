941bbf123222d9c2600efc3e3f3d7c270818ab7f
it27427 <istiak.tushar.qtec@gmail.com>
2023-02-20 10:10:54 +0600

    first commit

---
+++ assets/js/nice-select/settings-select.js
@@ -0,0 +1,24 @@
+NiceSelect.bind(document.getElementById("select-industry"), {
+  searchable: false,
+  placeholder: 'Software and IT'
+});
+
+NiceSelect.bind(document.getElementById("settings-country"), {
+  searchable: false,
+  placeholder: 'Bangladesh'
+});
+
+NiceSelect.bind(document.getElementById("settings-city"), {
+  searchable: false,
+  placeholder: 'Dhaka'
+});
+
+NiceSelect.bind(document.getElementById("settings-timezone"), {
+  searchable: false,
+  placeholder: 'Development'
+});
+
+NiceSelect.bind(document.getElementById("job-stage"), {
+  searchable: false,
+  placeholder: 'Design'
+});
